2838c3c23ac78fcd1070fd3b622b5ff9960c8062
Diego Avila Ramos <145205871+avila46@users.noreply.github.com>
2023-12-04 20:30:43 +0000

    prueba error

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,61 +1,51 @@
 
 import React, { useState} from "react";
+import { LoaderTargetPlugin } from "webpack";
 
 //create your first component
 const Home = () => {
 	
 	const [entrada, setEntrada] = useState ("")
-	const [contenido, setContenido] = useState(["hola","adios"])
-
+	const [contenido, setContenido] = useState ([])
+	const [hoverIndex, setHoverIndex] = useState (null)
+ 
 	function capturarEntrada(event) {
 		setEntrada(event.target.value)
 	}
 	function capturarContenido(event) {
-		if (event.keyCode===13){
+		if (event.keyCode===13 && entrada!= ""){
 			setContenido(contenido.concat(entrada))
-		}
-		
-		
+		}	
 	}
-	// const mostrarCerrar = ""
-	// function ocultarCerrar() {
-	// 	mostrarCerrar = "hidden"
-	// }
-	// const handleRemoveTask = (task) => {
-	// 	// Elimina la tarea del estado
-	// 	setTasks(tasks.filter((t) => t !== task));
-	//   };
-	const [contenidos, setContenidos] = useState([])
-	const eliminarDato = (contenido) => { 
-		setContenidos(contenidos.filter((i) => i !== contenido))
+	const mostrarBoton = (index) =>{
+		// console.log("hola");
+		// setHoverIndex(index)
 	}
-
-{/* <ul>
-        {tasks.map((task) => (
-          <li key={task}>
-            {task}
-            <button onClick={() => handleRemoveTask(task)}>
-              Eliminar
-            </button>
-          </li>
-        ))}
-      </ul> */}
-
-
+	const ocultarBoton = () =>{
+		setHoverIndex(null)
+	}
+	const eliminarDato = (index) => { 
+		setContenido(contenido.filter((elemento,i) => i !== index))
+	}
 	const listaContenido = contenido.map((elemento, index) =>
- 	    <li key={index}>
-   	 		{elemento}
-			<button type="button" className={`btn-close`} aria-label="Close" onClick={() => eliminarDato(index)}></button>
+		<li key={index} className="list-group-item border-bottom">
+			<div className="d-flex justify-content-between">
+				{elemento}
+				<button type="button" className={`btn btn-outline-light ${hoverIndex===index ? "d-flex" : "d-none"}`} onMouseEnter={() => mostrarBoton(index)} onMouseLeave={ocultarBoton} onClick={() => eliminarDato(index)} >x</button>
+			</div>
   	 	</li>
 	 );
-  
-	
+	 const actualizacionContenido =  () =>{
+		return contenido.length
+	 }
 	return (
 
 		<div className="w-50 mx-auto mt-5" onKeyDown={capturarContenido} >
-			<input type="text" className="form-control" onChange={capturarEntrada} />
-			<ul className="list-group list-group-flush">
+			<h1 className="text-center w-50 mx-auto">Lista de Tareas</h1>
+			<input type="text" className="form-control mx-auto" onChange={capturarEntrada} />
+			<ul className="list-group-flush border-end m-3">
 				{listaContenido}
+				<li className="list-group-item border-bottom">Tienes {actualizacionContenido()} items</li>
 	  		</ul>
 		</div>
 		
